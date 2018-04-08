44b3e6916d81b687fff2822d5a2092f39d9fdf75
jjvein <jjvein@gmail.com>
2018-04-08 23:31:02 +0800

    ci

--- js/content-script.js
+++ js/content-script.js
@@ -1,6 +1,4 @@
-﻿console.log('这是content script!');
-
-// 注意，必须设置了run_at=document_start 此段代码才会生效
+﻿// 注意，必须设置了run_at=document_start 此段代码才会生效
 document.addEventListener('DOMContentLoaded', function()
 {
 	// 注入自定义JS
@@ -23,15 +21,6 @@ document.addEventListener('DOMContentLoaded', function()
 			var temp = document.createElement('style');
 			temp.id = 'my_custom_css';
 			(document.head || document.body).appendChild(temp);
-			var css = "
-			/* 移除百度右侧广告 */
-			#content_right{display:none;}
-			/* 覆盖整个屏幕的相关推荐 */
-			.rrecom-btn-parent{display:none;}'
-			/* 难看的按钮 */
-			.result-op.xpath-log{display:none !important;}";
-			temp.innerHTML = css;
-			console.log('已注入自定义CSS！');
 			// 屏蔽百度推广信息
 			removeAdByJs();
 			// 这种必须用JS移除的广告一般会有延迟，干脆每隔一段时间清楚一次
